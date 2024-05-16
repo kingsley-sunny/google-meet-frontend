50d5cf9d4f85abc877a0ae793d9c3cde2d18b248
kingsley-sunny <ezeobisunny51@gmail.com>
2024-05-16 17:15:30 +0100

    feat: design the responsiveness of the meeting page (not yet complete)

--- components/Pages/MeetingPage/MeetingPage.tsx
+++ components/Pages/MeetingPage/MeetingPage.tsx
@@ -1,8 +1,10 @@
-import { MicOffIcon, UserIcon } from "lucide-react";
+import { MicOffIcon, MinusIcon, PlusIcon, UserIcon } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
 import { BsInfoCircle } from "react-icons/bs";
 import { ImPhoneHangUp } from "react-icons/im";
 import { LuUsers2 } from "react-icons/lu";
 import { MdOutlineMessage, MdOutlineScreenShare } from "react-icons/md";
+import { cn } from "../../../lib/utils";
 import MicIcon from "../../Icons/MicIcon";
 import VideoIcon from "../../Icons/VideoIcon";
 import VideoSlashIcon from "../../Icons/VideoSlashIcon";
@@ -13,33 +15,141 @@ const MeetingPage = () => {
   const { audioRef, isAudioEnabled, isVideoOpen, mediaStream, toggleMic, toggleVideo, videoRef } =
     useVideoRecord();
 
+  const [isScreenShare, setIsScreenShare] = useState<boolean>(false);
+  const [screenStream, setScreenStream] = useState<MediaStream>();
+  const screenShareRef = useRef<HTMLVideoElement>(null);
+
+  console.log("🚀 ~~ MeetingPage ~~ screenStream:", screenStream);
+
+  const [counter, setCounter] = useState<number>(1);
+
+  const incrementCounter = () => {
+    if (counter < 14) {
+      setCounter(counter + 1);
+    }
+  };
+
+  const decrementCounter = () => {
+    if (counter > 1) {
+      setCounter(counter - 1);
+    }
+  };
+
+  const toggleScreenShare = async () => {
+    if (!isScreenShare) {
+      const mediaScreenStream = await window.navigator.mediaDevices.getDisplayMedia({
+        audio: false,
+        video: true,
+      });
+
+      setScreenStream(mediaScreenStream);
+      setIsScreenShare(true);
+      return;
+    }
+
+    setScreenStream(undefined);
+    setIsScreenShare(false);
+  };
+
+  useEffect(() => {
+    console.log("🚀 ~~ useEffect ~~ screenStream:", screenStream);
+    if (screenShareRef.current && screenStream) {
+      screenShareRef.current.srcObject = screenStream;
+
+      (screenStream as MediaStream & { oninactive: (...val: any) => any }).oninactive = () => {
+        toggleScreenShare();
+      };
+    }
+  }, [isScreenShare, screenStream]);
+
   return (
-    <div className='bg-black/90 relative h-screen text-white px-8 py-4'>
-      <div className=' h-[calc(100%-4rem)] flex gap-4 justify-center items-center w-full'>
-        {/* the user */}
-        {!isVideoOpen ? (
-          <div className='relative bg-white/10 rounded-xl w-full h-[calc(100%-20rem)] max-w-6xl mx-auto flex justify-center items-center'>
-            <Avatar className='w-32 h-32'>
-              <AvatarImage src='https://github.com/shadcn.png' />
-              <AvatarFallback>
-                <UserIcon className='w-28 h-2' />
-              </AvatarFallback>
-            </Avatar>
+    <div className='bg-black/90 flex relative h-screen text-white px-8 py-4'>
+      <div
+        className={cn(
+          "border-2 py-16 h-full flex flex-wrap gap-4 items-center w-full",
+          counter === 2 && "grid grid-rows-2 sm:grid-cols-2 sm:grid-rows-1 sm:place-items-center",
+          counter === 3 && "grid grid-rows-3 sm:grid-cols-2 sm:grid-rows-2",
+          counter === 4 && "grid grid-rows-4 sm:grid-cols-2 sm:grid-rows-2",
 
-            <p className='text-lg absolute bottom-4 left-4'>John Doe</p>
-          </div>
-        ) : null}
+        )}
+      >
+        {!isVideoOpen
+          ? Array.from({ length: counter }).map((_, index) => (
+              <div
+                key={_ as any}
+                className='min-w-[200px] border-2 py-6 px-4 mx-auto h-full w-full relative bg-white/10 rounded-xl flex justify-center items-center'
+              >
+                <Avatar className='w-12 h-12 lg:w-32 lg:h-32'>
+                  <AvatarImage src='https://github.com/shadcn.png' />
+                  <AvatarFallback>
+                    <UserIcon className='w-28 h-2' />
+                  </AvatarFallback>
+                </Avatar>
 
-        {isVideoOpen ? (
-          <div className='relative bg-white/10 rounded-xl w-full h-[calc(100%-20rem)] max-w-6xl mx-auto flex justify-center items-center overflow-hidden'>
-            <video
-              ref={videoRef}
-              autoPlay
-              width={"100%"}
-              id='videoElement'
-              className='w-full h-full'
-              muted
-            />
+                <p className='text-sm lg:text-lg absolute bottom-4 left-4'>John Doe</p>
+
+                <div className='absolute top-10 right-10'>
+                  {isAudioEnabled ? (
+                    <MicIcon className='w-4 h-4 lg:w-6 lg:h-6' />
+                  ) : (
+                    <MicOffIcon className='w-4 h-4 lg:w-6 lg:h-6' />
+                  )}
+                </div>
+              </div>
+            ))
+          : null}
+
+        {isVideoOpen
+          ? Array.from({ length: counter }).map((_, index) => (
+              <div
+                key={_ as any}
+                className={cn(
+                  "min-w-[200px] border-2 py-6 px-4 mx-auto h-full w-full relative bg-white/10 rounded-xl flex justify-center items-center overflow-hidden"
+                  // counter === 1 && "max-h-full h-1/2 xl:h-full",
+                  // counter === 1 && "max-h-full h-1/2 xl:h-full",
+                )}
+              >
+                <div className='relative flex h-full'>
+                  <video
+                    ref={videoRef}
+                    autoPlay
+                    width={"100%"}
+                    id='videoElement'
+                    className='w-full min-w-64 min-h-32 h-full'
+                    muted
+                  />
+                  <div className='absolute top-0 h-32 w-full bg-gradient-to-b from-black/20 to-transparent'></div>
+                </div>
+
+                <p className='text-sm lg:text-lg absolute bottom-4 left-4'>John Doe</p>
+
+                <div className='absolute top-10 right-10'>
+                  {isAudioEnabled ? (
+                    <MicIcon className='w-4 h-4 lg:w-6 lg:h-6' />
+                  ) : (
+                    <MicOffIcon className='w-4 h-4 lg:w-6 lg:h-6' />
+                  )}
+                </div>
+              </div>
+            ))
+          : null}
+
+        {isScreenShare ? (
+          <div className='min-w-[200px] py-6 mx-auto relative bg-white/10 rounded-xl flex justify-center items-center overflow-hidden px-10'>
+            <div className='relative flex h-full'>
+              <video ref={screenShareRef} autoPlay width={"100%"} className='w-full h-full' muted />
+              <div className='absolute top-0 h-32 w-full bg-gradient-to-b from-black/20 to-transparent'></div>
+            </div>
+
+            <p className='text-sm lg:text-lg absolute bottom-4 left-4'>John Doe</p>
+
+            <div className='absolute top-10 right-10'>
+              {isAudioEnabled ? (
+                <MicIcon className='w-4 h-4 lg:w-6 lg:h-6' />
+              ) : (
+                <MicOffIcon className='w-4 h-4 lg:w-6 lg:h-6' />
+              )}
+            </div>
           </div>
         ) : null}
       </div>
@@ -47,7 +157,7 @@ const MeetingPage = () => {
       <audio ref={audioRef} autoPlay muted></audio>
 
       {/* The bottom nav */}
-      <div className=' flex justify-between '>
+      <div className='fixed w-full bottom-0 flex justify-between '>
         <div className='hidden lg:flex space-x-4 items-center '>
           <p className=''>{new Date().toDateString()}</p>
 
@@ -89,16 +199,24 @@ const MeetingPage = () => {
           )}
 
           <button className='w-12 h-12 bg-white/15 rounded-full flex justify-center items-center'>
-            <MdOutlineScreenShare className='w-5 h-5' />
+            <MdOutlineScreenShare className='w-5 h-5' onClick={toggleScreenShare} />
           </button>
 
           <button className='w-12 h-12 bg-white/15 rounded-full flex justify-center items-center'>
             <ImPhoneHangUp className='w-5 h-5' />
           </button>
+
+          <button className='w-12 h-12 bg-white/15 rounded-full flex justify-center items-center'>
+            <PlusIcon className='w-5 h-5' onClick={incrementCounter} />
+          </button>
+
+          <button className='w-12 h-12 bg-white/15 rounded-full flex justify-center items-center'>
+            <MinusIcon className='w-5 h-5' onClick={decrementCounter} />
+          </button>
         </div>
 
         {/* THE SETTINGS AND THE CHAT ICONS */}
-        <div className='flex space-x-4 items-center'>
+        <div className='lg:flex space-x-4 items-center hidden'>
           <button className='w-12 h-12 rounded-full flex justify-center items-center'>
             <BsInfoCircle className='w-6 h-6' />
           </button>
